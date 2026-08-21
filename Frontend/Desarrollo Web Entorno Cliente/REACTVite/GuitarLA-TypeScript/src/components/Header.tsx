345cbf83e47d8a6980a4057da94fc0abd7323e40
Nacho SLKN <Nacho15111997@gmail.com>
2026-08-21 16:46:11 +0200

    Corrige despliegue y portada de GuitarLA

--- Frontend/Desarrollo Web Entorno Cliente/REACTVite/GuitarLA-TypeScript/src/components/Header.tsx
+++ Frontend/Desarrollo Web Entorno Cliente/REACTVite/GuitarLA-TypeScript/src/components/Header.tsx
@@ -25,13 +25,13 @@ export default function Header({
         <div className="row justify-content-center justify-content-md-between">
           <div className="col-8 col-md-3">
             <a href="#">
-              <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
+              <img className="img-fluid" src="./img/logo.svg" alt="imagen logo" />
             </a>
           </div>
 
           <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
             <div className="carrito">
-              <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
+              <img className="img-fluid" src="./img/carrito.png" alt="imagen carrito" />
 
               <div id="carrito" className="bg-white p-3">
                 {isEmpty ? (
@@ -54,7 +54,7 @@ export default function Header({
                             <td>
                               <img
                                 className="img-fluid"
-                                src={`/img/${guitar.image}.jpg`}
+                                src={`${import.meta.env.BASE_URL}img/${guitar.image}.jpg`}
                                 alt="imagen guitarra"
                               />
                             </td>
